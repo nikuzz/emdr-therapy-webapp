efcf726a32baca856cd36a5b2885f2dda00ae296
Georgi Petrov <ggeorgip@gmail.com>
2018-07-27 20:21:04 +0100

    Code align

--- emdr.js
+++ emdr.js
@@ -69,105 +69,153 @@ angular.module("app", [])
                 }
             };
         }
+
         $scope.getCoord = function (get, c) {
-            if (get() == 0)
+            if (get() == 0) {
                 return ($scope.playgrd[c].delta / 2) + $scope.playgrd[c].min;
-            else if (get() == 1)
+            } else if (get() == 1) {
                 return $scope.playgrd[c].min;
-            else
+            } else {
                 return $scope.playgrd[c].max;
+            }
         }
+
         $scope.getStartPoint = function () {
             var pX, pY;
             pX = $scope.getCoord(getX, 'x');
             pY = $scope.getCoord(getY, 'y');
             return new Point(pX, pY);
         }
+
         $scope.init = function () {
             $scope.movement.dirty = false;
             $scope.movement.running = false;
-            if ($scope.circle)
+            if ($scope.circle) {
                 $scope.circle.remove();
+            }
             $scope.circle = undefined;
             $scope.redrawBg();
             $scope.redrawCircle({
                 reset: true
             });
         };
+
         $scope.redrawCircle = function (arg) {
+
             $scope.updatePlayground();
             var pos;
             if ($scope.circle) {
                 pos = $scope.circle.position;
                 $scope.circle.remove();
             }
-            if (!pos || (arg && arg.reset))
+
+            if (!pos || (arg && arg.reset)) {
                 pos = $scope.getStartPoint();
-            if (pos.x < $scope.playgrd.x.min) pos.x += ($scope.playgrd.x.min - pos.x);
-            else if (pos.x > $scope.playgrd.x.max) pos.x -= (pos.x - $scope.playgrd.x.max);
-            if (pos.y < $scope.playgrd.y.min) pos.y += ($scope.playgrd.y.min - pos.y);
-            else if (pos.y > $scope.playgrd.y.max) pos.y -= (pos.y - $scope.playgrd.y.max);
+            }
+
+            if (pos.x < $scope.playgrd.x.min) {
+                pos.x += ($scope.playgrd.x.min - pos.x);
+            } else if (pos.x > $scope.playgrd.x.max) {
+                pos.x -= (pos.x - $scope.playgrd.x.max);
+            }
+
+            if (pos.y < $scope.playgrd.y.min) {
+                pos.y += ($scope.playgrd.y.min - pos.y);
+            } else if (pos.y > $scope.playgrd.y.max) {
+                pos.y -= (pos.y - $scope.playgrd.y.max);
+            }
+
             $scope.circle = new Path.Circle(pos, $scope.conf.radius.value);
-            if (!$scope.color) $scope.color = $scope.conf.defaultColor;
+
+            if (!$scope.color) {
+                $scope.color = $scope.conf.defaultColor;
+            }
+
             document.getElementById('color').value = ($scope.color);
             document.getElementById('color').style.backgroundColor = $scope.color;
             $scope.circle.fillColor = $scope.color;
         };
+
         $scope.redrawBg = function () {
-            if (bgRectangle)
+            if (bgRectangle) {
                 bgRectangle.remove();
-            if (!$scope.bgcolor) $scope.bgcolor = $scope.conf.defaultBgColor;
+            }
+
+            if (!$scope.bgcolor) {
+                $scope.bgcolor = $scope.conf.defaultBgColor;
+            }
+
             document.getElementById('bgcolor').value = ($scope.bgcolor);
             document.getElementById('bgcolor').style.backgroundColor = $scope.bgcolor;
+
             bgRectangle = new Path.Rectangle({
                 point: [0, 0],
                 size: [view.size.width, view.size.height],
                 fillColor: $scope.bgcolor,
                 selected: true
             });
+
             bgRectangle.sendToBack();
-        }
+        };
+
         $scope.start = function () {
             $scope.movement.running = true;
             $scope.movement.dirty = true;
-        }
+        };
+
         $scope.stop = function () {
             $scope.movement.running = false;
-        }
+        };
+
         $scope.isRunning = function () {
             return $scope.movement.running;
-        }
+        };
+
         $scope.isDirty = function () {
             return $scope.movement.dirty;
-        }
+        };
+
         $scope.toggleRun = function () {
             if ($scope.isRunning()) $scope.stop();
             else $scope.start();
-        }
+        };
+
         view.onFrame = function (event) {
-            if (!$scope.movement.running) return;
+            if (!$scope.movement.running) {
+                return;
+            }
+
             if ($scope.debug && event.count % 10 == 0) {
                 $scope.event = event;
                 $scope.$apply(); // aggiorno i dati esposti per il DEBUG
             }
+
             if (getX() !== 0) {
-                if ($scope.movement.v == getX() && $scope.circle.position.x < $scope.playgrd.x.max)
+                if ($scope.movement.v == getX() && $scope.circle.position.x < $scope.playgrd.x.max) {
                     $scope.circle.position.x += $scope.conf.velocity.value * $scope.playgrd.x.delta / $scope.conf.frameDisplacement;
-                else if ($scope.movement.v == -getX() && $scope.circle.position.x > $scope.playgrd.x.min)
+                } else if ($scope.movement.v == -getX() && $scope.circle.position.x > $scope.playgrd.x.min) {
                     $scope.circle.position.x -= $scope.conf.velocity.value * $scope.playgrd.x.delta / $scope.conf.frameDisplacement;
-                else $scope.movement.v = -$scope.movement.v;
+                }
+                else {
+                    $scope.movement.v = -$scope.movement.v;
+                }
             }
             if (getY() !== 0) {
-                if ($scope.movement.v == getY() && $scope.circle.position.y < $scope.playgrd.y.max)
+                if ($scope.movement.v == getY() && $scope.circle.position.y < $scope.playgrd.y.max) {
                     $scope.circle.position.y += $scope.conf.velocity.value * $scope.playgrd.y.delta / $scope.conf.frameDisplacement;
-                else if ($scope.movement.v == -getY() && $scope.circle.position.y > $scope.playgrd.y.min)
+                } else if ($scope.movement.v == -getY() && $scope.circle.position.y > $scope.playgrd.y.min) {
                     $scope.circle.position.y -= $scope.conf.velocity.value * $scope.playgrd.y.delta / $scope.conf.frameDisplacement;
-                else if (getX() === 0) // inverto il senso di marcia solo non è già stato fatto per x
+                } else if (getX() === 0) {
                     $scope.movement.v = -$scope.movement.v;
+                }
             }
-        }
+        };
+
         view.onKeyUp = function (e) {
-            if ($scope.debug) console.log(e.key, e);
+            if ($scope.debug) {
+                console.log(e.key, e);
+            }
+
             if (['space', 'enter'].indexOf(e.key) >= 0) {
                 $scope.toggleRun();
                 $scope.$apply();
@@ -181,10 +229,12 @@ angular.module("app", [])
                 $scope.conf.velocity.value -= $scope.conf.velocity.step;
                 $scope.$apply();
             }
-        }
+        };
+
         $scope.init();
         paper.view.draw();
-        // Gestione della lingua
+
+        // Language management
         $scope.lang = $scope.conf.lang;
         $scope.trans = trans;
-    }])
+    }]);
