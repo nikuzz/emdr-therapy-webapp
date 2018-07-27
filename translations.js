bd8152242ab9718a6cf3510960e885c4cd56b5d7
Georgi Petrov <ggeorgip@gmail.com>
2018-07-27 20:15:20 +0100

    Move out translations

---
+++ translations.js
@@ -0,0 +1,69 @@
+function appTranslations() {
+    return {
+        en: {
+            label: 'English',
+            colors: 'Colors',
+            dimension: 'Dimension',
+            movement: 'Movement',
+            horizontal: 'Horizontal',
+            vertical: 'Vertical',
+            oblique1: 'Oblique 1',
+            oblique2: 'Oblique 2',
+            speed: 'Speed',
+            start: 'Start',
+            stop: 'Stop'
+        },
+        de: {
+            label: 'Deutsch',
+            colors: 'Farben',
+            dimension: 'Dimension',
+            movement: 'Bewegung',
+            horizontal: 'Horizontal',
+            vertical: 'Vertikal',
+            oblique1: 'Schräg 1',
+            oblique2: 'Schräg 2',
+            speed: 'Geschwindigkeit',
+            start: 'Anfang',
+            stop: 'Halt'
+        },
+        fr: {
+            label: 'Français',
+            colors: 'Couleurs',
+            dimension: 'Dimension',
+            movement: 'Mouvement',
+            horizontal: 'Horizontal',
+            vertical: 'Verticale',
+            oblique1: 'Oblique 1',
+            oblique2: 'Oblique 2',
+            speed: 'Vélocité',
+            start: 'Démarrer',
+            stop: 'Arrêtez'
+        },
+        it: {
+            label: 'Italiano',
+            colors: 'Colori',
+            dimension: 'Dimensione',
+            movement: 'Movimento',
+            horizontal: 'Orizzontale',
+            vertical: 'Verticale',
+            oblique1: 'Obliquo 1',
+            oblique2: 'Obliquo 2',
+            speed: 'Velocità',
+            start: 'Partenza',
+            stop: 'Stop'
+        },
+        es: {
+            label: 'Español',
+            colors: 'Colores',
+            dimension: 'Dimensión',
+            movement: 'Movimiento',
+            horizontal: 'Horizontal',
+            vertical: 'Vertical',
+            oblique1: 'Oblicua 1',
+            oblique2: 'Oblicua 2',
+            speed: 'Velocidad',
+            start: 'Inicia',
+            stop: 'Para'
+        }
+    }
+}
